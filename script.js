df8e9f2f3071ce2f9877fedc1dfcdb839de5c24c
asood <avanisood0806@gmail.com>
2025-04-12 11:18:33 +0530

    bangaloreeee

--- script.js
+++ script.js
@@ -29,5 +29,32 @@ document.addEventListener("DOMContentLoaded", () => {
   
   window.addEventListener('scroll', showOnScroll);
   window.addEventListener('load', showOnScroll);
+
+  function classify(element){
+    if(element.tagname === 'IMG') return 'image';
+    if(element.tagname === 'INPUT' && element.type === 'text') return 'text input';
+    if(element.tagname === 'TEXTAREA') return 'text area';
+    if(element.tagname === 'SELECT') return 'drop-down';
+    if(element.tagname === 'BUTTON') return 'button';
+    if(element.tagname === 'A') return 'link';
+    if(element.tagname.startsWith('H')) return 'heading';
+    if(element.tagname === 'P') return 'paragraph';
+    if(element.tagname === 'DIV') return 'div container';
+    return element.tagName.toLowerCase();
+  }
+
+    window.addEventListener('load', () => {
+      const timestamp = new Date().toISOString();
+      console.log(`${timestamp} , view , entire page`);
+    });
+  
+    // Log click events
+    document.addEventListener('click', function(event) {
+      const timestamp = new Date().toISOString();
+      const element = event.target;
+      const classification = classify(element);
+      console.log(`${timestamp} , click , ${classification}`);
+    });
+  
   
   
